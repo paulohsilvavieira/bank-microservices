b4fec5bf8be50c4f936d23e421ac60d001fae72b
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2022-09-29 12:48:39 -0300

    feat: add process rabbitmq publisher

--- create-user-service/app/controllers/create_user_controller.rb
+++ create-user-service/app/controllers/create_user_controller.rb
@@ -1,25 +1,29 @@
 class CreateUserController < ApplicationController
   def create
     user = User.create(user_params)
-
     if user.save
+      publish_confirm_email_service
       return render json: {
-        msg: 'user created'
-      }, status: 201
+        msg: 'user created and email confirmation sended'
+      }, status: :created
     end
 
-    render json: { errors: user.errors.to_json }, status: 400
+    render json: { errors: user.errors }, status: :bad_request
   end
 
   private
 
   def user_params
-    params.require(:user).permit(
+    params.permit(
       :first_name,
       :second_name,
       :email,
       :document,
       :document_type
     )
+  end
+
+  def publish_confirm_email_service
+    PublisherService.publish_direct(user_params, 'bank.direct.sendconfirmmail', 'sendconfirmmail.service')
   end
 end
