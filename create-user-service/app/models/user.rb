ce2ad4812c4f82a7a29a794c13e4266440344a07
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2022-11-14 19:08:28 -0300

    chore: remove ruby

--- create-user-service/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :email, presence: true
-  validates :second_name, presence: true
-  validates :document, presence: true
-  validates :document_type, presence: true
-end
