5f4fb49386070de54212bafd617d54403fff41ee
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2022-11-15 12:55:08 -0300

    feat: mensagem de erro para autenticação

---
+++ auth-service/src/domain/errors/auth-error.ts
@@ -0,0 +1,8 @@
+
+export class AuthentitcationError extends Error {
+  constructor () {
+    super('Auth Error - User Invalid')
+    this.name = 'AUTH_ERROR_01'
+    this.stack = 'AUTH_ERROR'
+  }
+}
