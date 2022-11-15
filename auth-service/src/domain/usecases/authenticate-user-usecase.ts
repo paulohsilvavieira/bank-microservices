ceddd285cf2e6b301d3c1a3ea56c0e978044e962
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2022-11-15 12:55:51 -0300

    feat: caso de uso para autenticar usuario

---
+++ auth-service/src/domain/usecases/authenticate-user-usecase.ts
@@ -0,0 +1,34 @@
+import { Encrypter } from '../contracts/crypto/encrypter'
+import { VerifyAccountRepo } from '../contracts/repositories/verify-account-repo'
+import { TokenGenerator } from '../contracts/token/token-generator'
+import { AuthenticateUser } from '../contracts/usecases/auth'
+import { AuthentitcationError } from '../errors/auth-error'
+
+export class AuthenticateUserUsecase implements AuthenticateUser {
+  constructor (
+    private readonly authRepo: VerifyAccountRepo,
+    private readonly crypto: Encrypter,
+    private readonly token: TokenGenerator) {}
+
+  async execute (params: AuthenticateUser.Params): Promise<AuthenticateUser.Result> {
+    const { encryptedText } = this.crypto.encrypt(params.password)
+
+    const { authId } = await this.authRepo.verifyAccount({
+      accountNumber: params.accountNumber,
+      password: encryptedText
+    })
+
+    if (!authId) {
+      return {
+        token: undefined,
+        error: new AuthentitcationError()
+      }
+    }
+    const { token } = this.token.generate(authId)
+
+    return {
+      token,
+      error: undefined
+    }
+  }
+}
