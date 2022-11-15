ce4030cc829445c1eb25de83f61df4db9213b750
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2022-11-15 12:55:30 -0300

    feat: protocolo de caso de uso de autenticar user

---
+++ auth-service/src/domain/contracts/usecases/auth.ts
@@ -0,0 +1,16 @@
+import { AuthentitcationError } from '../../errors/auth-error'
+
+export interface AuthenticateUser {
+  execute: (params: AuthenticateUser.Params) => Promise<AuthenticateUser.Result>
+}
+
+export namespace AuthenticateUser {
+  export type Params={
+    accountNumber: string
+    password: string
+  }
+  export type Result ={
+    token: string | null
+    error: AuthentitcationError | null
+  }
+}
